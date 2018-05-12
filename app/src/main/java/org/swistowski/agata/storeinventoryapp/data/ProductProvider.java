a3b27540fa196b570678128be49e83280667ee44
Agata <agata@swistowski.org>
2018-05-12 23:57:57 +0100

    zmiany meza na mezowym branchu

--- app/src/main/java/org/swistowski/agata/storeinventoryapp/data/ProductProvider.java
+++ app/src/main/java/org/swistowski/agata/storeinventoryapp/data/ProductProvider.java
@@ -88,7 +88,10 @@ public class ProductProvider extends ContentProvider {
      */
     private Uri insertProduct(Uri uri, ContentValues values) {
 
-        validateName(values);
+        String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null || name.equals("")) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
 
         Integer price = values.getAsInteger(ProductEntry.COLUMN_PRODUCT_PRICE);
         if (price != null && price < 0){
@@ -124,13 +127,6 @@ public class ProductProvider extends ContentProvider {
         return ContentUris.withAppendedId(uri, id);
     }
 
-    private void validateName(ContentValues values) {
-        String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
-        if (name == null || name.equals("")) {
-            throw new IllegalArgumentException("Product requires a name");
-        }
-    }
-
     @Override
     public int update(Uri uri, ContentValues contentValues, String selection,
                       String[] selectionArgs) {
@@ -149,7 +145,10 @@ public class ProductProvider extends ContentProvider {
 
     private int updateProduct(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         if (values.containsKey(ProductEntry.COLUMN_PRODUCT_NAME)) {
-            validateName(values);
+            String name = values.getAsString(ProductEntry.COLUMN_PRODUCT_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException("Product requires a name");
+            }
         }
 
         if (values.containsKey(ProductEntry.COLUMN_PRODUCT_PRICE)) {
